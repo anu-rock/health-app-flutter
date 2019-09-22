86827119064d8d937f55bedab34056b60ef8d371
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-22 06:53:42 +0530

    Add home screen title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'For today',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      'September 22, 2019',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
