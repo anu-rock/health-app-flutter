4f8087286371cd126e3421c8902f3c4e48693a34
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-22 10:04:53 +0530

    Add basic UI structure to HydrationScreen

--- lib/screens/hydration_screen.dart
+++ lib/screens/hydration_screen.dart
@@ -10,8 +10,48 @@ class HydrationScreen extends StatelessWidget {
         backgroundColor: kSwatchBlueColor,
         elevation: 0.0,
       ),
-      body: SafeArea(
-        child: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.opacity,
+            size: 80.0,
+            color: Colors.white,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            'Hydration',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40.0,
+            ),
+          ),
+          Text(
+            '4 of 12',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17.0,
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
