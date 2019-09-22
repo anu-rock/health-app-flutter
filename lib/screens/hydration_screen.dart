52acf2f924de6eaf19a894630a2aec9bbfbef7cb
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-22 09:34:54 +0530

    Add HydrationScreen
    This screen will be displayed on tap of cups tile on HomeScreen.

---
+++ lib/screens/hydration_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HydrationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF52BEFF),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF52BEFF),
+        elevation: 0.0,
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
